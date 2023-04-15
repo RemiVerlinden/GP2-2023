c63afd63d709cd88cd05ed3e292dee629bc495dd
RemiVerlinden <remi.verlinden@outlook.com>
2023-04-15 15:25:36 +0200

    finished the modelanimator and finished week6

--- OverlordEngine/OverlordEngine/Misc/ModelAnimator.cpp
+++ OverlordEngine/OverlordEngine/Misc/ModelAnimator.cpp
@@ -1,102 +1,107 @@
 #include "stdafx.h"
 #include "ModelAnimator.h"
-
-ModelAnimator::ModelAnimator(MeshFilter* pMeshFilter):
-	m_pMeshFilter{pMeshFilter}
+ModelAnimator::ModelAnimator(MeshFilter* pMeshFilter) :
+	m_pMeshFilter{ pMeshFilter }
 {
 	SetAnimation(0);
 }
 
-void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
+void ModelAnimator::Update(const SceneContext& sceneContext)
 {
-	TODO_W6_();
-
-	//We only update the transforms if the animation is running and the clip is set
 	if (m_IsPlaying && m_ClipSet)
 	{
-		//1. 
-		//Calculate the passedTicks (see the lab document)
-		//auto passedTicks = ...
-		//Make sure that the passedTicks stay between the m_CurrentClip.Duration bounds (fmod)
+		float passedTicks = fmodf(sceneContext.pGameTime->GetElapsed() * m_CurrentClip.ticksPerSecond * m_AnimationSpeed, m_CurrentClip.duration);
+		m_TickCount = fmodf((m_Reversed ? m_TickCount - passedTicks : m_TickCount + passedTicks) + m_CurrentClip.duration, m_CurrentClip.duration);
 
-		//2. 
-		//IF m_Reversed is true
-		//	Subtract passedTicks from m_TickCount
-		//	If m_TickCount is smaller than zero, add m_CurrentClip.Duration to m_TickCount
-		//ELSE
-		//	Add passedTicks to m_TickCount
-		//	if m_TickCount is bigger than the clip duration, subtract the duration from m_TickCount
+		AnimationKey keyA{}, keyB{};
+		for (int i = 0; i < m_CurrentClip.keys.size(); ++i)
+		{
+			if (m_CurrentClip.keys[i].tick > m_TickCount)
+			{
+				keyB = m_CurrentClip.keys[i];
+				keyA = m_CurrentClip.keys[i - 1];
+				break;
+			}
+		}
 
-		//3.
-		//Find the enclosing keys
-		AnimationKey keyA, keyB;
-		//Iterate all the keys of the clip and find the following keys:
-		//keyA > Closest Key with Tick before/smaller than m_TickCount
-		//keyB > Closest Key with Tick after/bigger than m_TickCount
+		float blendFactor = (m_TickCount - keyA.tick) / (keyB.tick - keyA.tick);
 
-		//4.
-		//Interpolate between keys
-		//Figure out the BlendFactor (See lab document)
-		//Clear the m_Transforms vector
-		//FOR every boneTransform in a key (So for every bone)
-		//	Retrieve the transform from keyA (transformA)
-		//	auto transformA = ...
-		// 	Retrieve the transform from keyB (transformB)
-		//	auto transformB = ...
-		//	Decompose both transforms
-		//	Lerp between all the transformations (Position, Scale, Rotation)
-		//	Compose a transformation matrix with the lerp-results
-		//	Add the resulting matrix to the m_Transforms vector
+		size_t boneCount = m_CurrentClip.keys[0].boneTransforms.size();
+		m_Transforms.resize(boneCount);
+
+		for (int i = 0; i < boneCount; ++i)
+		{
+			XMMATRIX transformA = XMLoadFloat4x4(&keyA.boneTransforms[i]);
+			XMMATRIX transformB = XMLoadFloat4x4(&keyB.boneTransforms[i]);
+
+			XMVECTOR scaleA, rotationA, translationA, scaleB, rotationB, translationB;
+			XMMatrixDecompose(&scaleA, &rotationA, &translationA, transformA);
+			XMMatrixDecompose(&scaleB, &rotationB, &translationB, transformB);
+
+			XMVECTOR scaleLerp = XMVectorLerp(scaleA, scaleB, blendFactor);
+			XMVECTOR rotationSlerp = XMQuaternionSlerp(rotationA, rotationB, blendFactor);
+			XMVECTOR translationLerp = XMVectorLerp(translationA, translationB, blendFactor);
+
+			XMMATRIX resultMatrix = XMMatrixScalingFromVector(scaleLerp) * XMMatrixRotationQuaternion(rotationSlerp) * XMMatrixTranslationFromVector(translationLerp);
+			XMStoreFloat4x4(&m_Transforms[i], resultMatrix);
+		}
 	}
 }
 
-void ModelAnimator::SetAnimation(const std::wstring& /*clipName*/)
+void ModelAnimator::SetAnimation(const std::wstring& clipName)
 {
-	TODO_W6_()
-	//Set m_ClipSet to false
-	//Iterate the m_AnimationClips vector and search for an AnimationClip with the given name (clipName)
-	//If found,
-	//	Call SetAnimation(Animation Clip) with the found clip
-	//Else
-	//	Call Reset
-	//	Log a warning with an appropriate message
+	m_ClipSet = false;
+	for (const AnimationClip& clip : m_pMeshFilter->GetAnimationClips())
+	{
+		if (clip.name == clipName)
+		{
+			SetAnimation(clip);
+			return;
+		}
+	}
+	Reset(false);
+	Logger::LogWarning(L"No animation clip has been found with clipname [" + clipName + L"]");
 }
 
-void ModelAnimator::SetAnimation(UINT /*clipNumber*/)
+void ModelAnimator::SetAnimation(UINT clipNumber)
 {
-	TODO_W6_()
-	//Set m_ClipSet to false
-	//Check if clipNumber is smaller than the actual m_AnimationClips vector size
-	//If not,
-		//	Call Reset
-		//	Log a warning with an appropriate message
-		//	return
-	//else
-		//	Retrieve the AnimationClip from the m_AnimationClips vector based on the given clipNumber
-		//	Call SetAnimation(AnimationClip clip)
+	m_ClipSet = false;
+	if (clipNumber < m_pMeshFilter->GetAnimationClips().size())
+	{
+		SetAnimation(m_pMeshFilter->GetAnimationClips().at(clipNumber));
+		return;
+	}
+	Reset(false);
+	Logger::LogWarning(L"No animation clip has been found with ID [" + std::to_wstring(clipNumber) + L"]");
 }
 
-void ModelAnimator::SetAnimation(const AnimationClip& /*clip*/)
+void ModelAnimator::SetAnimation(const AnimationClip& clip)
 {
-	TODO_W6_()
-	//Set m_ClipSet to true
-	//Set m_CurrentClip
-
-	//Call Reset(false)
+	m_ClipSet = true;
+	m_CurrentClip = clip;
+	Reset(false);
 }
 
-void ModelAnimator::Reset(bool /*pause*/)
+void ModelAnimator::Reset(bool pause)
 {
-	TODO_W6_()
-	//If pause is true, set m_IsPlaying to false
+	if (pause)	m_IsPlaying = false;
 
-	//Set m_TickCount to zero
-	//Set m_AnimationSpeed to 1.0f
+	m_TickCount = 0;
+	m_AnimationSpeed = 1.0f;
 
-	//If m_ClipSet is true
-	//	Retrieve the BoneTransform from the first Key from the current clip (m_CurrentClip)
-	//	Refill the m_Transforms vector with the new BoneTransforms (have a look at vector::assign)
-	//Else
-	//	Create an IdentityMatrix 
-	//	Refill the m_Transforms vector with this IdenityMatrix (Amount = BoneCount) (have a look at vector::assign)
+	if (m_ClipSet)
+	{
+		m_Transforms = m_CurrentClip.keys[0].boneTransforms;
+	}
+	else
+	{
+		const XMFLOAT4X4 identity
+		{
+			1.f, 0.f, 0.f, 0.f,
+			0.f, 1.f, 0.f, 0.f,
+			0.f, 0.f, 1.f, 0.f,
+			0.f, 0.f, 0.f, 1.f
+		};
+		m_Transforms.assign(m_pMeshFilter->m_BoneCount, identity);
+	}
 }

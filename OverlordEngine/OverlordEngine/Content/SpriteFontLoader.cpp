09ba8aa1f3158b5703f3cf2b20ec0d83fd46b577
RemiVerlinden <remi.verlinden@outlook.com>
2023-04-20 23:35:47 +0200

    finished picking and font, now I will start with characterScene

--- OverlordEngine/OverlordEngine/Content/SpriteFontLoader.cpp
+++ OverlordEngine/OverlordEngine/Content/SpriteFontLoader.cpp
@@ -1,95 +1,229 @@
 #include "stdafx.h"
 #include "SpriteFontLoader.h"
 
+//SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
+//{
+//	const auto pReader = new BinaryReader();
+//	pReader->Open(loadInfo.assetFullPath);
+//
+//	if (!pReader->Exists())
+//	{
+//		Logger::LogError(L"Failed to read the assetFile!\nPath: \'{}\'", loadInfo.assetSubPath);
+//		return nullptr;
+//	}
+//
+//	TODO_W7(L"Implement SpriteFontLoader >> Parse .fnt file")
+//	//See BMFont Documentation for Binary Layout
+//
+//	//Parse the Identification bytes (B,M,F)
+//	//If Identification bytes doesn't match B|M|F,
+//	//Log Error (SpriteFontLoader::LoadContent > Not a valid .fnt font) &
+//	//return nullptr
+//	//...
+//
+//	//Parse the version (version 3 required)
+//	//If version is < 3,
+//	//Log Error (SpriteFontLoader::LoadContent > Only .fnt version 3 is supported)
+//	//return nullptr
+//	//...
+//
+//	//Valid .fnt file >> Start Parsing!
+//	//use this SpriteFontDesc to store all relevant information (used to initialize a SpriteFont object)
+//	SpriteFontDesc fontDesc{};
+//
+//	//**********
+//	// BLOCK 0 *
+//	//**********
+//	//Retrieve the blockId and blockSize
+//	//Retrieve the FontSize [fontDesc.fontSize]
+//	//Move the binreader to the start of the FontName [BinaryReader::MoveBufferPosition(...) or you can set its position using BinaryReader::SetBufferPosition(...))
+//	//Retrieve the FontName [fontDesc.fontName]
+//	//...
+//
+//	//**********
+//	// BLOCK 1 *
+//	//**********
+//	//Retrieve the blockId and blockSize
+//	//Retrieve Texture Width & Height [fontDesc.textureWidth/textureHeight]
+//	//Retrieve PageCount
+//	//> if pagecount > 1
+//	//	> Log Error (Only one texture per font is allowed!)
+//	//Advance to Block2 (Move Reader)
+//	//...
+//
+//	//**********
+//	// BLOCK 2 *
+//	//**********
+//	//Retrieve the blockId and blockSize
+//	//Retrieve the PageName (BinaryReader::ReadNullString)
+//	//Construct the full path to the page texture file
+//	//	>> page texture should be stored next to the .fnt file, pageName contains the name of the texture file
+//	//	>> full texture path = asset parent_path of .fnt file (see loadInfo.assetFullPath > get parent_path) + pageName (filesystem::path::append)
+//	//	>> Load the texture (ContentManager::Load<TextureData>) & Store [fontDesc.pTexture]
+//
+//	//**********
+//	// BLOCK 3 *
+//	//**********
+//	//Retrieve the blockId and blockSize
+//	//Retrieve Character Count (see documentation)
+//	//Create loop for Character Count, and:
+//	//> Retrieve CharacterId (store Local) and cast to a 'wchar_t'
+//	//> Create instance of FontMetric (struct)
+//	//	> Set Character (CharacterId) [FontMetric::character]
+//	//	> Retrieve Xposition (store Local)
+//	//	> Retrieve Yposition (store Local)
+//	//	> Retrieve & Set Width [FontMetric::width]
+//	//	> Retrieve & Set Height [FontMetric::height]
+//	//	> Retrieve & Set OffsetX [FontMetric::offsetX]
+//	//	> Retrieve & Set OffsetY [FontMetric::offsetY]
+//	//	> Retrieve & Set AdvanceX [FontMetric::advanceX]
+//	//	> Retrieve & Set Page [FontMetric::page]
+//	//	> Retrieve Channel (BITFIELD!!!) 
+//	//		> See documentation for BitField meaning [FontMetrix::channel]
+//	//	> Calculate Texture Coordinates using Xposition, Yposition, fontDesc.TextureWidth & fontDesc.TextureHeight [FontMetric::texCoord]
+//	//
+//	//> Insert new FontMetric to the metrics [font.metrics] map
+//	//	> key = (wchar_t) charId
+//	//	> value = new FontMetric
+//	//(loop restarts till all metrics are parsed)
+//
+//	//Done!
+//	delete pReader;
+//	return new SpriteFont(fontDesc);
+//}
+
 SpriteFont* SpriteFontLoader::LoadContent(const ContentLoadInfo& loadInfo)
 {
+	//************
+	// OPEN FILE *
+	//************
 	const auto pReader = new BinaryReader();
 	pReader->Open(loadInfo.assetFullPath);
-
 	if (!pReader->Exists())
 	{
 		Logger::LogError(L"Failed to read the assetFile!\nPath: \'{}\'", loadInfo.assetSubPath);
 		return nullptr;
 	}
 
-	TODO_W7(L"Implement SpriteFontLoader >> Parse .fnt file")
-	//See BMFont Documentation for Binary Layout
+	//***********
+	// ID BYTES *
+	//***********
+	std::string idBytes{};
+	for (int i = 0; i < 3; ++i) { idBytes.push_back(pReader->Read<char>()); }
+	//if bytes don't match BMF identification characters, exit early
+	if (idBytes != "BMF")
+	{
+		Logger::LogError(L"SpriteFontLoader::LoadContent > Not a valid .fnt font");
+		return nullptr;
+	}
 
-	//Parse the Identification bytes (B,M,F)
-	//If Identification bytes doesn't match B|M|F,
-	//Log Error (SpriteFontLoader::LoadContent > Not a valid .fnt font) &
-	//return nullptr
-	//...
+	//****************
+	// VERSION BYTES *
+	//****************
+	const int versionByte = pReader->Read<char>();
+	//if version byte is not 3, warn user about not being compatible with other versions
+	if (versionByte != 3)
+	{
+		Logger::LogError(L"SpriteFontLoader::LoadContent > Non-supported BMF version, use version 3!");
+		return nullptr;
+	}
 
-	//Parse the version (version 3 required)
-	//If version is < 3,
-	//Log Error (SpriteFontLoader::LoadContent > Only .fnt version 3 is supported)
-	//return nullptr
-	//...
-
-	//Valid .fnt file >> Start Parsing!
-	//use this SpriteFontDesc to store all relevant information (used to initialize a SpriteFont object)
-	SpriteFontDesc fontDesc{};
+	//*************
+	// PARSE FONT *
+	//*************
+	//create empty font data structure
+	SpriteFontDesc fontDescription{};
 
 	//**********
 	// BLOCK 0 *
 	//**********
-	//Retrieve the blockId and blockSize
-	//Retrieve the FontSize [fontDesc.fontSize]
-	//Move the binreader to the start of the FontName [BinaryReader::MoveBufferPosition(...) or you can set its position using BinaryReader::SetBufferPosition(...))
-	//Retrieve the FontName [fontDesc.fontName]
-	//...
+	//skip blockId(1 byte) and blockSize(4 bytes)
+	pReader->MoveBufferPosition(5);
+	//read font size
+	fontDescription.fontSize = pReader->Read<short>();
+	//move the binary reader to the start of the font name
+	pReader->MoveBufferPosition(12);
+	//read font name
+	fontDescription.fontName = pReader->ReadNullString();
 
 	//**********
 	// BLOCK 1 *
 	//**********
-	//Retrieve the blockId and blockSize
-	//Retrieve Texture Width & Height [fontDesc.textureWidth/textureHeight]
-	//Retrieve PageCount
-	//> if pagecount > 1
-	//	> Log Error (Only one texture per font is allowed!)
-	//Advance to Block2 (Move Reader)
-	//...
+	//skip block id(1), block size(4), lineHeight(2) and base(2) values
+	pReader->MoveBufferPosition(9);
+	//read texture width & height
+	fontDescription.textureWidth = pReader->Read<short>();
+	fontDescription.textureHeight = pReader->Read<short>();
+	//read page count
+	const short pageCount = pReader->Read<short>();
+	//if pageCount exceeds 1, warn user that we only support 1 texture per font
+	if (pageCount > 1)
+	{
+		Logger::LogError(L"SpriteFontLoader::LoadContent > page count above 1, we only support single texture fonts!");
+		return nullptr;
+	}
+	//skip the rest of the data in this block
+	pReader->MoveBufferPosition(5);
+
 
 	//**********
 	// BLOCK 2 *
 	//**********
-	//Retrieve the blockId and blockSize
-	//Retrieve the PageName (BinaryReader::ReadNullString)
-	//Construct the full path to the page texture file
-	//	>> page texture should be stored next to the .fnt file, pageName contains the name of the texture file
-	//	>> full texture path = asset parent_path of .fnt file (see loadInfo.assetFullPath > get parent_path) + pageName (filesystem::path::append)
-	//	>> Load the texture (ContentManager::Load<TextureData>) & Store [fontDesc.pTexture]
+	//skip block id(1) and block size(4)
+	pReader->MoveBufferPosition(5);
+	//read page name
+	const std::wstring pageName = pReader->ReadNullString();
+	//calculate full path of texture file
+	const std::filesystem::path texturePath = loadInfo.assetFullPath.parent_path().append(pageName);
+	//load the texture
+	fontDescription.pTexture = ContentManager::Load<TextureData>(texturePath);
 
 	//**********
 	// BLOCK 3 *
 	//**********
-	//Retrieve the blockId and blockSize
-	//Retrieve Character Count (see documentation)
-	//Create loop for Character Count, and:
-	//> Retrieve CharacterId (store Local) and cast to a 'wchar_t'
-	//> Create instance of FontMetric (struct)
-	//	> Set Character (CharacterId) [FontMetric::character]
-	//	> Retrieve Xposition (store Local)
-	//	> Retrieve Yposition (store Local)
-	//	> Retrieve & Set Width [FontMetric::width]
-	//	> Retrieve & Set Height [FontMetric::height]
-	//	> Retrieve & Set OffsetX [FontMetric::offsetX]
-	//	> Retrieve & Set OffsetY [FontMetric::offsetY]
-	//	> Retrieve & Set AdvanceX [FontMetric::advanceX]
-	//	> Retrieve & Set Page [FontMetric::page]
-	//	> Retrieve Channel (BITFIELD!!!) 
-	//		> See documentation for BitField meaning [FontMetrix::channel]
-	//	> Calculate Texture Coordinates using Xposition, Yposition, fontDesc.TextureWidth & fontDesc.TextureHeight [FontMetric::texCoord]
-	//
-	//> Insert new FontMetric to the metrics [font.metrics] map
-	//	> key = (wchar_t) charId
-	//	> value = new FontMetric
-	//(loop restarts till all metrics are parsed)
+	//skip block id(1)
+	pReader->MoveBufferPosition(1);
+	//get block size and calculate character count (each character is described in 20 bytes)
+	const int blockSize = pReader->Read<int>();
+	const int characterCount = blockSize / 20;
+	//loop over every character
+	for (int i = 0; i < characterCount; ++i)
+	{
+		//create empty font metric data struct
+		FontMetric characterData{};
+
+		//read character id
+		const wchar_t characterId = static_cast<wchar_t>(pReader->Read<unsigned int>());
+		characterData.character = characterId;
+		//read x & y position
+		unsigned short xPosition = pReader->Read<unsigned short>();
+		unsigned short yPosition = pReader->Read<unsigned short>();
+		//read width & height
+		characterData.width = pReader->Read<unsigned short>();
+		characterData.height = pReader->Read<unsigned short>();
+		//read x & y offsets
+		characterData.offsetX = pReader->Read<short>();
+		characterData.offsetY = pReader->Read<short>();
+		//read x advance
+		characterData.advanceX = pReader->Read<short>();
+		//read page number
+		characterData.page = pReader->Read<unsigned char>();
+		//read channel
+		characterData.channel = pReader->Read<char>();
+		//convert to our data format (bitflag value => index of rgba vector)
+		std::unordered_map<char, char> conversionMap{ {1,2},{2,1},{4,0},{8,3} };
+		characterData.channel = conversionMap.at(characterData.channel);
+		//calculate texture coordinates in [0,1] range
+		characterData.texCoord = XMFLOAT2(float(xPosition) / float(fontDescription.textureWidth), float(yPosition) / float(fontDescription.textureHeight));
+
+		//add character info to character info map
+		fontDescription.metrics.insert({ characterId, characterData });
+	}
+
 
 	//Done!
 	delete pReader;
-	return new SpriteFont(fontDesc);
+	return new SpriteFont(fontDescription);
 }
 
 void SpriteFontLoader::Destroy(SpriteFont* objToDestroy)
--- OverlordEngine/OverlordProject/Resources/Effects/TextRenderer.fx
+++ OverlordEngine/OverlordProject/Resources/Effects/TextRenderer.fx
@@ -1,24 +1,26 @@
+//INCOMPLETE!
+
 float4x4 gTransform : WORLDVIEWPROJECTION;
 Texture2D gSpriteTexture;
-float2 gTextureSize = 16;
+float2 gTextureSize;
 
 SamplerState samPoint
 {
-    Filter = MIN_MAG_MIP_POINT;
-    AddressU = WRAP;
-    AddressV = WRAP;
+	Filter = MIN_MAG_MIP_POINT;
+	AddressU = WRAP;
+	AddressV = WRAP;
 };
 
-BlendState EnableBlending 
-{     
+BlendState EnableBlending
+{
 	BlendEnable[0] = TRUE;
 	SrcBlend = SRC_ALPHA;
-    DestBlend = INV_SRC_ALPHA;
+	DestBlend = INV_SRC_ALPHA;
 };
 
-RasterizerState BackCulling 
-{ 
-	CullMode = BACK; 
+RasterizerState BackCulling
+{
+	CullMode = BACK;
 };
 
 //SHADER STRUCTS
@@ -37,7 +39,7 @@ struct GS_DATA
 	float4 Position : SV_POSITION; //HOMOGENEOUS clipping space position
 	float4 Color: COLOR; //Color of the vertex
 	float2 TexCoord: TEXCOORD0; //Texcoord of the vertex
-	int Channel: TEXCOORD1; //Channel of the vertex
+	int Channel : TEXCOORD1; //Channel of the vertex
 };
 
 //VERTEX SHADER
@@ -52,67 +54,71 @@ VS_DATA MainVS(VS_DATA input)
 void CreateVertex(inout TriangleStream<GS_DATA> triStream, float3 pos, float4 col, float2 texCoord, int channel)
 {
 	//Create a new GS_DATA object
+	GS_DATA output;
 	//Fill in all the fields
+	output.Position =mul(float4(pos,1.0f), gTransform);
+	output.Color = col;
+	output.TexCoord = texCoord;
+	output.Channel = channel;
 	//Append it to the TriangleStream
-	GS_DATA vertexData = (GS_DATA)0;
-	
-	vertexData.Position = mul(float4(pos, 1), gTransform);
-	vertexData.Color = col;
-	vertexData.TexCoord = texCoord;
-	vertexData.Channel = channel;
-	
-	triStream.Append(vertexData);
+	triStream.Append(output);
 }
 
 [maxvertexcount(4)]
 void MainGS(point VS_DATA vertex[1], inout TriangleStream<GS_DATA> triStream)
 {
-	//REMOVE THIS >
-	//GS_DATA dummyData = (GS_DATA) 0; //Just some dummy data
-	//triStream.Append(dummyData); //The geometry shader needs to emit something, see what happens if it doesn't emit anything.
-	//< STOP REMOVING
-
 	//Create a Quad using the character information of the given vertex
 	//Note that the Vertex.CharSize is in screenspace, TextureCoordinates aren't ;) [Range 0 > 1]
 
-	float3 pos = vertex[0].Position;
-	float2 tex = vertex[0].TexCoord;
-	float2 size = vertex[0].CharSize;
-	float4 color = vertex[0].Color;
-	float2 texSize = float2(size.x / gTextureSize.x,size.y / gTextureSize.y);
-	int channel = vertex[0].Channel;
+	float3 cornerPos;
+	float2 cornerTexCoord;
+	float2 charSizeTexCoord; 
+	charSizeTexCoord.x = vertex[0].CharSize.x / gTextureSize.x;
+	charSizeTexCoord.y = vertex[0].CharSize.y / gTextureSize.y;
 	
 	//1. Vertex Left-Top
-	CreateVertex(triStream, pos, color, tex, channel);
+	cornerPos = vertex[0].Position;
+	cornerTexCoord= vertex[0].TexCoord;
+	CreateVertex(triStream, cornerPos, vertex[0].Color, cornerTexCoord, vertex[0].Channel);
 
-	//2. Vertex Right-Top
-	CreateVertex(triStream, float3(pos.x + size.x, pos.y, pos.z), color, float2(tex.x + texSize.x, tex.y), channel);
+	// 2. Vertex Right-Top
+	cornerPos.x += vertex[0].CharSize.x;
+	cornerTexCoord.x += charSizeTexCoord.x;
+	CreateVertex(triStream, cornerPos, vertex[0].Color, cornerTexCoord, vertex[0].Channel);
 
-	//3. Vertex Left-Bottom
-	CreateVertex(triStream, float3(pos.x, pos.y + size.y, pos.z), color, float2(tex.x, tex.y + texSize.y), channel);
+	// 3. Vertex Left-Bottom
 
-	//4. Vertex Right-Bottom
-	CreateVertex(triStream, float3(pos.x + size.x, pos.y + size.y, pos.z), color, float2(tex.x + texSize.x, tex.y + texSize.y), channel);
+	cornerPos += float3(-vertex[0].CharSize.x,vertex[0].CharSize.y,0);
+	cornerTexCoord += float2(-charSizeTexCoord.x, charSizeTexCoord.y);
+	CreateVertex(triStream, cornerPos, vertex[0].Color, cornerTexCoord, vertex[0].Channel);
+
+	// 4. Vertex Right-Bottom
+	cornerPos.x += vertex[0].CharSize.x;
+	cornerTexCoord.x += charSizeTexCoord.x;
+	CreateVertex(triStream, cornerPos, vertex[0].Color, cornerTexCoord, vertex[0].Channel);
 }
 
 //PIXEL SHADER
 //************
-float4 MainPS(GS_DATA input) : SV_TARGET 
-{
+float4 MainPS(GS_DATA input) : SV_TARGET{
+
 	//Sample the texture and return the correct channel [Vertex.Channel]
 	//You can iterate a float4 just like an array, using the index operator
 	//Also, don't forget to colorize ;) [Vertex.Color]
+	float4 sampledColor = gSpriteTexture.Sample(samPoint,input.TexCoord);
+	float channelColor = sampledColor[input.Channel];
 
-	return gSpriteTexture.Sample(samPoint, input.TexCoord)[input.Channel] * input.Color;
+	float4 color = channelColor > 0.0 ? channelColor * input.Color : float4(0, 0, 0, 0);
+	
+	return color;
 }
 
 // Default Technique
-technique10 Default 
-{
-	pass p0 
-	{
+technique10 Default {
+
+	pass p0 {
 		SetRasterizerState(BackCulling);
-		SetBlendState(EnableBlending,float4(0.0f, 0.0f, 0.0f, 0.0f), 0xFFFFFFFF);
+		SetBlendState(EnableBlending, float4(0.0f, 0.0f, 0.0f, 0.0f), 0xFFFFFFFF);
 		SetVertexShader(CompileShader(vs_4_0, MainVS()));
 		SetGeometryShader(CompileShader(gs_4_0, MainGS()));
 		SetPixelShader(CompileShader(ps_4_0, MainPS()));
